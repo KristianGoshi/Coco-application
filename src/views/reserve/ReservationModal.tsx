9d266c44629f0e8de86e17676bf8f29de8371e0a
KristianGoshi <goshikristian@gmail.com>
2022-06-16 18:58:13 +0200

    rregullimi i reservation modal

--- src/views/reserve/ReservationModal.tsx
+++ src/views/reserve/ReservationModal.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {useCallback, useState} from 'react';
 import {
+  Keyboard,
   Pressable,
   StyleSheet,
   Text,
@@ -15,6 +16,7 @@ import DatePicker from 'react-native-date-picker';
 import moment from 'moment';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { setReservations } from '../../redux/actions/reserveActions';
+import KeyboardAwareContainer from '../../components/Keyboard';
 
 export interface ReservationModalProps {
   closeModal: any
@@ -26,6 +28,7 @@ const ReservationModal: React.FC<ReservationModalProps> = React.memo(({closeModa
 
   const [name, setName] = useState('');
   const [date, setDate] = useState(new Date);
+  const [oldDate, setOldDate] = useState(date);
   const [nrPers, setPers] = useState('');
   const [open, setOpen] = useState(false);
   const falseDate = moment(new Date).format('MMMM Do YYYY, h:mm a');
@@ -41,89 +44,103 @@ const ReservationModal: React.FC<ReservationModalProps> = React.memo(({closeModa
   };
 
   const createReservation = useCallback(async () => {
-    closeModal(false);
-    await dispatch(setReservations([{emri: name, data: moment(date).format('MMMM Do YYYY, h:mm a'), nrPersonave: nrPers}]))
+    if (name != '' && nrPers != '' && date != oldDate) {
+      closeModal(false);
+      await dispatch(
+        setReservations([
+          {
+            emri: name,
+            data: moment(date).format('MMMM Do YYYY, h:mm a'),
+            nrPersonave: nrPers,
+          },
+        ]),
+      );
+    }
   }, [name, date, nrPers]);
 
   return (
-    <View style={styles.centeredView}>
-      <View style={styles.modalView}>
-        <Text style={styles.modalTitle}>{t('modal.title')}</Text>
-        <View style={styles.textInput}>
-          <TextInput
-            placeholder={t('modal.name')}
-            autoCapitalize="none"
-            autoCorrect={false}
-            label={t('modal.name')}
-            onChangeText={text => changeText(text, 'name')}
-          />
-        </View>
-        <View style={[styles.textInput, {marginTop: -10}]}>
-          <TextInput
-            placeholder={t('modal.persona')}
-            autoCapitalize="none"
-            autoCorrect={false}
-            label={t('modal.persona')}
-            onChangeText={text => changeText(text, 'persona')}
-          />
-        </View>
-        <TouchableOpacity
-          style={{flexDirection: 'row', marginTop: 10}}
-          onPress={() => setOpen(true)}>
-          <View>
-            <Icon
-              name="calendar-alt"
-              size={30}
-              style={{color: APP_COLORS.background.extra}}
+    <KeyboardAwareContainer>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalTitle}>{t('modal.title')}</Text>
+          <View style={styles.textInput}>
+            <TextInput
+              placeholder={t('modal.name')}
+              autoCapitalize="none"
+              autoCorrect={false}
+              label={t('modal.name')}
+              onChangeText={text => changeText(text, 'name')}
             />
           </View>
-          <View style={{marginTop: 8, marginLeft: 15}}>
-            <Text style={{color: APP_COLORS.background.extra}}>
-              {moment(date).format('MMMM Do YYYY, h:mm a') != falseDate
-                ? moment(date).format('MMMM Do YYYY, h:mm a')
-                : t('modal.chooseDate')}
-            </Text>
+          <View style={[styles.textInput, {marginTop: -10}]}>
+            <TextInput
+              placeholder={t('modal.persona')}
+              autoCapitalize="none"
+              autoCorrect={false}
+              label={t('modal.persona')}
+              onChangeText={text => changeText(text, 'persona')}
+              keyboardType={'number-pad'}
+            />
           </View>
-        </TouchableOpacity>
-        <View>
-          <DatePicker
-            modal
-            open={open}
-            date={date}
-            onConfirm={date => {
-              setOpen(false);
-              setDate(date);
-            }}
-            onCancel={() => {
-              setOpen(false);
-            }}
-          />
-        </View>
-        <View style={{flexDirection: 'row', marginTop: 30}}>
-          <Pressable
-            style={[
-              styles.modalButton,
-              {marginRight: 5, backgroundColor: 'white'},
-            ]}
-            onPress={() => closeModal(false)}>
-            <Text style={[styles.textStyle, {marginTop: 8}]}>
-              {t('modal.cancel')}
-            </Text>
-          </Pressable>
-          <Pressable
-            style={[styles.modalButton, {marginLeft: 5}]}
-            onPress={() => createReservation()}>
-            <Text
+          <TouchableOpacity
+            style={{flexDirection: 'row', marginTop: 10}}
+            onPress={() => setOpen(true)}>
+            <View>
+              <Icon
+                name="calendar-alt"
+                size={30}
+                style={{color: APP_COLORS.background.extra}}
+              />
+            </View>
+            <View style={{marginTop: 8, marginLeft: 15}}>
+              <Text style={{color: APP_COLORS.background.extra}}>
+                {moment(date).format('MMMM Do YYYY, h:mm a') != falseDate
+                  ? moment(date).format('MMMM Do YYYY, h:mm a')
+                  : t('modal.chooseDate')}
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <View>
+            <DatePicker
+              modal
+              open={open}
+              date={date}
+              onConfirm={date => {
+                setOpen(false);
+                setDate(date);
+              }}
+              onCancel={() => {
+                setOpen(false);
+              }}
+              minimumDate={new Date()}
+            />
+          </View>
+          <View style={{flexDirection: 'row', marginTop: 30}}>
+            <Pressable
               style={[
-                styles.textStyle,
-                {marginTop: 8, color: APP_COLORS.background.extra},
-              ]}>
-              {t('modal.confirm')}
-            </Text>
-          </Pressable>
+                styles.modalButton,
+                {marginRight: 5, backgroundColor: 'white'},
+              ]}
+              onPress={() => closeModal(false)}>
+              <Text style={[styles.textStyle, {marginTop: 8}]}>
+                {t('modal.cancel')}
+              </Text>
+            </Pressable>
+            <Pressable
+              style={[styles.modalButton, {marginLeft: 5}]}
+              onPress={() => createReservation()}>
+              <Text
+                style={[
+                  styles.textStyle,
+                  {marginTop: 8, color: APP_COLORS.background.extra},
+                ]}>
+                {t('modal.confirm')}
+              </Text>
+            </Pressable>
+          </View>
         </View>
       </View>
-    </View>
+    </KeyboardAwareContainer>
   );
 });
 
